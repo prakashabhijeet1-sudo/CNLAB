ed7968a42a723399435db62e471791584480addc
prakashabhijeet1-sudo <prakashabhijeet1@gmail.com>
2026-09-01 20:41:17 +0530

    cn lab codes

--- DAY06/q3.c
+++ DAY06/q3.c
@@ -1,2 +1,33 @@
-/*
+/*// addition of three numbners
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int sockfd, len, k;
+    int a, b, c, sum;
+    char buf[100], result[100];
+
+    struct sockaddr_in sa, ca;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sa.sin_port = htons(6027);
+
+    bind(sockfd, (struct sockaddr *)&sa, sizeof(sa));
+
+    printf("Server waiting...\n");
+
+    len = sizeof(ca);
+
+    k = recvfrom(sockfd, buf, 100, 0,
+                 (struct sockaddr *)&ca, &len);
+
+    buf[k] = '\0';
 */
