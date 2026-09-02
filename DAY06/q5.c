61f2bc94c3795f8ee1f3b0a75a171780da4d519c
prakashabhijeet1-sudo <prakashabhijeet1@gmail.com>
2026-09-02 18:04:54 +0530

    cn lab codes

---
+++ DAY06/q5.c
@@ -0,0 +1,35 @@
+/*      // calulator
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main()
+{
+    int sockfd, len, k;
+    float a, b, result;
+    char op;
+    char buf[100], ans[100];
+
+    struct sockaddr_in sa, ca;
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    sa.sin_family = AF_INET;
+    sa.sin_addr.s_addr = inet_addr("127.0.0.1");
+    sa.sin_port = htons(6027);
+
+    bind(sockfd, (struct sockaddr *)&sa, sizeof(sa));
+
+    printf("Server waiting...\n");
+
+    len = sizeof(ca);
+
+    k = recvfrom(sockfd, buf, 100, 0,
+                 (struct sockaddr *)&ca, &len);
+
+    buf[k] = '\0';
+                    
+*/
